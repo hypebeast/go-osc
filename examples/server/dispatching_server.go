ae2738224b6bf4368d7f9b8147474af1f1dd94aa
Sebastian Ruml <sebastian.ruml@gmail.com>
2015-02-25 19:27:46 +0100

    add new server example with handler functions

---
+++ examples/server/dispatching_server.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/hypebeast/go-osc/osc"
+
+func main() {
+	addr := "127.0.0.1:8765"
+	server := &osc.Server{Addr: addr}
+
+	server.Handle("/message/address", func(msg *osc.Message) {
+		osc.PrintMessage(msg)
+	})
+
+	server.ListenAndServe()
+}
