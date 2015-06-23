1a19715bfc07a30658f3ff42ed6a336ffc3ac542
Kate Ward <kate.ward@forestent.com>
2015-06-24 01:56:58 +0200

    listen on all interfaces, and standard TouchOSC port

--- examples/dispatching_server/server.go
+++ examples/dispatching_server/server.go
@@ -3,7 +3,7 @@ package main
 import osc "github.com/kward/go-osc"
 
 func main() {
-	addr := "127.0.0.1:8765"
+	addr := "0.0.0.0:8000"
 	server := &osc.Server{Addr: addr}
 
 	server.Handle("/message/address", func(msg *osc.Message) {
