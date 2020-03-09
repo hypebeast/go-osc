c1be8b29d8729f425556fc3bbf772c52ac2cb9f7
Dave Yarwood <dave.yarwood@gmail.com>
2020-03-09 12:53:09 -0400

    improve "basic server" example script for better debugging
    * Better debug output: nesting levels are indicated by indentation.
    
    * Support debugging arbitrary levels of nested bundles.
    
    * Use the higher-level server.ListenAndServe and a Dispatcher instance.

--- examples/basic_server/basic_server.go
+++ examples/basic_server/basic_server.go
@@ -1,67 +1,101 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
+	"math/rand"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/hypebeast/go-osc/osc"
 )
 
-func main() {
-	addr := "127.0.0.1:8765"
-	server := &osc.Server{}
-	conn, err := net.ListenPacket("udp", addr)
-	if err != nil {
-		fmt.Println("Couldn't listen: ", err)
+func indent(str string, indentLevel int) string {
+	indentation := strings.Repeat("  ", indentLevel)
+
+	result := ""
+
+	for i, line := range strings.Split(str, "\n") {
+		if i != 0 {
+			result += "\n"
+		}
+
+		result += indentation + line
 	}
-	defer conn.Close()
+
+	return result
+}
+
+func debug(packet osc.Packet, indentLevel int) string {
+	switch packet.(type) {
+	default:
+		return "Unknown packet type!"
+
+	case *osc.Message:
+		return fmt.Sprintf("-- OSC Message: %s", packet.(*osc.Message))
+
+	case *osc.Bundle:
+		bundle := packet.(*osc.Bundle)
+
+		result := fmt.Sprintf("-- OSC Bundle (%s):", bundle.Timetag.Time())
+
+		for i, message := range bundle.Messages {
+			result += "\n" + indent(
+				fmt.Sprintf("-- OSC Message #%d: %s", i+1, message),
+				indentLevel+1,
+			)
+		}
+
+		for _, bundle := range bundle.Bundles {
+			result += "\n" + indent(debug(bundle, 0), indentLevel+1)
+		}
+
+		return result
+	}
+}
+
+// Debugger is a simple Dispatcher that prints all messages and bundles as they
+// are received.
+type Debugger struct{}
+
+// Dispatch implements Dispatcher.Dispatch by printing the packet received.
+func (Debugger) Dispatch(packet osc.Packet) {
+	if packet != nil {
+		fmt.Println(debug(packet, 0) + "\n")
+	}
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s PORT\n", os.Args[0])
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	numArgs := len(os.Args[1:])
+
+	if numArgs != 2 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	port, err := strconv.ParseInt(os.Args[1], 10, 32)
+	if err != nil {
+		fmt.Println(err)
+		printUsage()
+		os.Exit(1)
+	}
+
+	addr := fmt.Sprintf("127.0.0.1:%d", port)
+
+	server := &osc.Server{Addr: addr, Dispatcher: Debugger{}}
 
 	fmt.Println("### Welcome to go-osc receiver demo")
-	fmt.Println("Press \"q\" to exit")
+	fmt.Printf("Listening via UDP on port %d...\n", port)
 
-	go func() {
-		fmt.Println("Start listening on", addr)
-
-		for {
-			packet, err := server.ReceivePacket(conn)
-			if err != nil {
-				fmt.Println("Server error: " + err.Error())
-				os.Exit(1)
-			}
-
-			if packet != nil {
-				switch packet.(type) {
-				default:
-					fmt.Println("Unknow packet type!")
-
-				case *osc.Message:
-					fmt.Printf("-- OSC Message: ")
-					osc.PrintMessage(packet.(*osc.Message))
-
-				case *osc.Bundle:
-					fmt.Println("-- OSC Bundle:")
-					bundle := packet.(*osc.Bundle)
-					for i, message := range bundle.Messages {
-						fmt.Printf("  -- OSC Message #%d: ", i+1)
-						osc.PrintMessage(message)
-					}
-				}
-			}
-		}
-	}()
-
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		c, err := reader.ReadByte()
-		if err != nil {
-			os.Exit(0)
-		}
-
-		if c == 'q' {
-			os.Exit(0)
-		}
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
